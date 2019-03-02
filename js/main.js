513067abee4bdf5ec023522f6455f3c30c76c4a2
sherkazy <sheki92m@mail.ru>
2019-03-02 17:45:12 +0600

    image path

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var main = new Vue({
     el: '#app',
     data: {
-        product: "hello"
+        product: "salam",
+        image1: 'assets/images/bir.jpg',
+        image2: 'assets/images/bir.jpg'
     }
 });
