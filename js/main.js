689bfc36338a2eb8c0a6c4b5f4ce340e85e410f1
sherkazy <sheki92m@mail.ru>
2019-03-02 18:19:01 +0600

    Methods, mouseOver

--- js/main.js
+++ js/main.js
@@ -3,6 +3,29 @@ var main = new Vue({
     data: {
         product: "salam",
         image1: 'assets/images/bir.jpg',
-        image2: 'assets/images/bir.jpg'
+        lubov: true,
+        datails: ['sexy', 'very sexy', 'very very sexy'],
+        variants: [
+            {
+                'id': '1',
+                'variant': 'variant1',
+                'imageVariant': 'assets/images/bir.jpg',
+            },
+            {
+                'id': '2',
+                'variant': 'variant2',
+                'imageVariant': 'assets/images/eki.jpg',
+            },
+            {
+                'id': '3',
+                'variant': 'variant3',
+                'imageVariant': 'assets/images/uch.jpg',
+            },
+        ]
+    },
+    methods: {
+        updateImage(image) {
+            this.image1 = image
+        }
     }
 });
